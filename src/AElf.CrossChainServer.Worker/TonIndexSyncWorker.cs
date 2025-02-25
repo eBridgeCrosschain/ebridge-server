326cccbcd97f23839fc1bf3d195d4dd1d64640c2
sherry <sherry@ebridge.exchange>
2025-02-25 18:31:45 +0800

    feat:change index bits.

--- src/AElf.CrossChainServer.Worker/TonIndexSyncWorker.cs
+++ src/AElf.CrossChainServer.Worker/TonIndexSyncWorker.cs
@@ -210,7 +210,7 @@ public class TonIndexSyncWorker : AsyncPeriodicBackgroundWorkerBase
         var receipt = bodySlice.LoadRef();
         var receiptSlice = receipt.Parse();
         var keyHash = receiptSlice.LoadBytes(32).ToHex();
-        var index = receiptSlice.LoadUInt(64);
+        var index = receiptSlice.LoadUInt(256);
         var receiptId = $"{keyHash}.{index}";
 
         var token = await _tokenAppService.GetAsync(new GetTokenInput
